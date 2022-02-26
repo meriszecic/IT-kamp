9217d15be77799bb2358e5279cb729c7c5bde0bb
Meris <meriszecic@gmail.com>
2022-02-26 16:57:54 +0100

    class and yt update

--- Cas/cas21.js
+++ Cas/cas21.js
@@ -1,24 +1,56 @@
-const cars = ["BMW", 123, false, "Volvo", "Saab", 12.5];
+// const cars = ["BMW", 123, false, "Volvo", "Saab", 12.5];
 
-cars.forEach((el, index) => {
-  console.log(el);
-  console.log(index);
-});
+// cars.forEach((el, index) => {
+//   console.log(el);
+//   console.log(index);
+// });
 
-function imeFunkcije(arg1, index) {
-  console.log(arg1);
-  console.log(index);
+// function imeFunkcije(arg1, index) {
+//   console.log(arg1);
+//   console.log(index);
+// }
+
+// function forEachCustom(arr, callbackfn) {
+//   for (let i = 0; i < arr.length; i++) {
+//     callbackfn(arr[i]);
+//   }
+// }
+
+// forEachCustom(cars, (arg1, index) => {
+//   console.log(arg1);
+//   index && console.log(index);
+// });
+
+//=================================================================
+//=================================================================
+
+function wash(callback) {
+  setTimeout(function () {
+    console.log("wash");
+    callback();
+  }, 3000);
+}
+function dry(callback) {
+  setTimeout(function () {
+    console.log("dry");
+    callback();
+  }, 2000);
+}
+function fold(callback) {
+  setTimeout(function () {
+    console.log("fold");
+    callback();
+  }, 1000);
 }
 
-function forEachCustom(arr, callbackfn) {
-  for (let i = 0; i < arr.length; i++) {
-    callbackfn(arr[i]);
-  }
+function doLaundy(action) {
+  action[0](() => {
+    action[1](() => {
+      action[2](() => {
+        console.log("Done");
+      });
+    });
+  });
 }
 
-forEachCustom(cars, (arg1, index) => {
-  console.log(arg1);
-  index && console.log(index);
-});
-
-
+doLaundy([wash, dry, fold]);
