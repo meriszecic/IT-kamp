dfe1be6d6b4c7a682063860316f6e70d50ecdccd
Meris <meriszecic@gmail.com>
2022-03-10 19:05:26 +0100

    cas

---
+++ Cas/cas25.js
@@ -0,0 +1,14 @@
+var prvi = document.createElement("div");
+prvi.style.border = "solid black 2px";
+prvi.style.height = "300px";
+prvi.style.width = "300px";
+prvi.style.backgroundColor = "red";
+prvi.style.borderRadius = "30px";
+prvi.style.display = "flex";
+prvi.style.justifyContent = "space-around";
+
+document.body.appendChild(prvi);
+document.body.appendChild(prvi);
+document.body.appendChild(prvi);
+document.body.appendChild(prvi);
+document.body.appendChild(prvi);
