c0428de08eb97684e913d5faaab863a46f261ece
Meris <meriszecic@gmail.com>
2022-03-17 19:07:31 +0100

    clas

---
+++ Cas/cas28.js
@@ -0,0 +1,14 @@
+let dugme = document.querySelector("#dugme");
+let email = document.querySelector("#email");
+let name = document.querySelector("#name");
+let password = document.querySelector("#password");
+let username = document.querySelector("#ime");
+let forma = document.querySelector("#forma");
+
+dugme.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(e);
+    objekat = {
+        
+    }
+});
