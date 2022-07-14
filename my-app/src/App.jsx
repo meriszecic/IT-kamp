9cb9fddfd0605bc89409882d4c7685e63d940971
Meris <meriszecic@gmail.com>
2022-07-14 18:20:42 +0200

    react first class

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./Card";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Apple />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
---
+++ my-app/src/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+  return (
+    <div className="Card" style={{ boder: "1px red solid" , backroundcolor: "blue" }}>
+      <h1> Ovo je Card </h1>
+      <p> Neki test</p>
+    </div>
+  );
+};
+
+export default Card;
