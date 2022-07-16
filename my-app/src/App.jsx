781b10d4dfc689ccf268fb9f1911a277fa633612
Meris <meriszecic@gmail.com>
2022-07-16 13:26:45 +0200

    react class 2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,30 @@
-import Card from "./Card";
 import "./App.css";
-
+import { useState } from "react";
 const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="container">
-      <Apple />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <button
+        className="dugme1"
+        onClick={() => {
+          setCounter(counter - 1);
+          console.log(counter);
+        }}
+      >
+        -1
+      </button>
+      <h1 className="broj">{counter}</h1>
+      <button
+        className="dugme1"
+        onClick={() => {
+          setCounter(counter + 1);
+          console.log(counter);
+        }}
+      >
+        +1
+      </button>
     </div>
   );
 };
+
+export default App;
